45a13589faf89006497b035a394ff6de35db4eca
PeppeS <pspfolio@gmail.com>
2016-04-10 15:10:15 +0300

    Basic userlist tempalte

--- src/app/user/user.controller.js
+++ src/app/user/user.controller.js
@@ -5,8 +5,25 @@
 		.module('app.user')
 		.controller('UserController', UserController);
 	
-	function UserController() {
+	UserController.$inject = ['$firebaseArray'];
+	
+	function UserController($firebaseArray) {
 		var vm = this;
-		vm.text = "plaa";
+		
+		var fireUsers = new Firebase('https://fiilarit.firebaseio.com/users');
+		
+		function User() {
+			this.id = null;
+			this.name = '';
+		}
+		
+		vm.user = new User();
+		vm.users = $firebaseArray(fireUsers);
+		vm.addUser = addUser;
+		
+		function addUser(user) {
+			vm.users.$add(user);
+			vm.user = new User();
+		}
 	}
 }());
