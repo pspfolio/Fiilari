fe4643b7422beeb9a162d1d102efea251bea8e0f
PeppeS <pspfolio@gmail.com>
2016-04-10 13:06:14 +0300

    Form for register user and logic to the controller

--- src/app/auth/auth.controller.js
+++ src/app/auth/auth.controller.js
@@ -5,8 +5,25 @@
 		.module('app.auth')
 		.controller('AuthController', AuthController);
 	
-	function AuthController() {
+	AuthController.$inject = ['$firebaseAuth'];
+	
+	function AuthController($firebaseAuth) {
 		var vm = this;
+		var firebaseReference = new Firebase('https://fiilarit.firebaseio.com/');
+		var firebaseAuthObject = $firebaseAuth(firebaseReference);
+		
+		vm.company = {
+			name: '',
+			email: '',
+			password: ''
+		};
+		
+		vm.register = register;
+		
+		function register(company) {
+			console.log(company);
+			firebaseAuthObject.$createUser(company);
+		}
 	}
 	
 }());
