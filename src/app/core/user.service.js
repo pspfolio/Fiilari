623609b2b8b1a40849ad038aadb3dec53af5989f
PeppeS <pspfolio@gmail.com>
2016-04-10 19:00:02 +0300

    coremodule and user service

---
+++ src/app/core/user.service.js
@@ -0,0 +1,16 @@
+(function() {
+	'use strict';
+	
+	angular
+		.module('app.core')
+		.factory('UserService', UserService);
+	
+	function UserService() {
+		
+		var service = {
+			
+		}
+		
+		return service;
+	}
+}());
