6f01062bf1a6ea6c59e9b1510902dd9de5d42831
PeppeS <pspfolio@gmail.com>
2016-04-11 19:05:33 +0300

    Userservice

--- src/app/core/user.service.js
+++ src/app/core/user.service.js
@@ -3,14 +3,39 @@
 	
 	angular
 		.module('app.core')
-		.factory('UserService', UserService);
+		.factory('userService', UserService);
 	
-	function UserService() {
+	UserService.$inject = ['$firebaseArray'];
+	
+	function UserService($firebaseArray) {
+		
+		var users = null;
 		
 		var service = {
-			
-		}
+			User: User,
+			getUserByCompany: getUsersByCompany,
+			reset: reset
+		};
 		
 		return service;
+		
+		function User() {
+			this.id = null;
+			this.name = '';
+		}
+		
+		//var fireUsers = new Firebase('https://fiilarit.firebaseio.com/users');
+		function getUsersByCompany(companyId) {
+			if(!users) {
+				users = $firebaseArray(new Firebase('https://fiilarit.firebaseio.com/users').child(companyId).child('users'));
+			}
+			
+			return users;
+		}
+		
+		function reset() {
+			users.$destroy();
+			users = null;
+		}
 	}
 }());
